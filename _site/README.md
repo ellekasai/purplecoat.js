04386c8f0de11a31b5250d2f7d344aaf562c566e
Elle Kasai <elle.kasai@gmail.com>
2017-05-25 10:07:21 -0700

    Remove Twitter

---
+++ _site/README.md
@@ -0,0 +1,27 @@
+Unfortunately, this project is **no longer maintained**.
+
+I now have other priorities in my life and no longer have time to update this library. Email me at elle.kasai@gmail.com if you'd like to reach me.
+
+# :womans_clothes: Purplecoat.js :womans_clothes:
+
+Purplecoat.js lets you create labeled overlays that can be triggered with a click. And you don't need to write a single line of Javascript.
+
+## [View Demo and Documentation &rarr;](http://ellekasai.github.io/purplecoat.js)
+
+![](http://f.cl.ly/items/10023K3W2A3F1y423x2c/Screen%20Recording%202014-11-09%20at%2000%3A38.gif)
+
+## Author & License
+
+Elle Kasai
+
+- [Website](http://ellekasai.com/about)
+- [Twitter](http://twitter.com/ellekasai)
+
+[MIT License](http://ellekasai.mit-license.org).
+
+### Special Thanks
+
+jQuery Plugin Help:
+
+- [@cg219](https://github.com/ellekasai/purplecoat.js/pull/8)
+- [@RobertLowe ](https://github.com/ellekasai/purplecoat.js/pull/6)
---
+++ _site/purplecoat.js
@@ -0,0 +1,78 @@
+(function ($) {
+
+  var f = function(purplecoatToggleData, purplecoatColorData, target) {
+    var $purplecoatVisible = $("[data-purplecoat-for=" + purplecoatToggleData + "]:visible");
+    var $purplecoatHidden = $("[data-purplecoat-for=" + purplecoatToggleData + "]:hidden");
+
+    if ($purplecoatVisible.length) {
+      $purplecoatVisible.fadeOut();
+    } else if ($purplecoatHidden.length) {
+      $purplecoatHidden.fadeIn();
+    } else {
+      target.each(function () {
+        var $myself = $(this);
+
+        if ($myself.is(":hidden")) {
+          return;
+        }
+
+        var $purplecoat = $('<div class="purplecoat"></div>');
+        $('body').append($purplecoat);
+
+        var $purplecoatInner = $('<div class="purplecoat-inner"></div>');
+        $purplecoat.append($purplecoatInner);
+
+        var $purplecoatInnerText = $('<div class="purplecoat-inner-text"></div>');
+        $purplecoatInnerText.html($myself.data('purplecoat-label'));
+        $purplecoatInner.append($purplecoatInnerText);
+
+        if ($myself.data('purplecoat-color')) {
+          purplecoatColorData = $myself.data('purplecoat-color');
+        }
+
+        if (purplecoatColorData) {
+          $purplecoat.css('background-color', purplecoatColorData);
+        }
+
+        $purplecoat
+        .attr('data-purplecoat-for', purplecoatToggleData)
+        .css({
+          'top': $myself.offset().top,
+          'left': $myself.offset().left,
+          'width': $myself.outerWidth(),
+          'height': $myself.outerHeight() })
+          .fadeIn();
+
+      });
+    }
+  }
+
+  $.fn.purplecoat = function() {
+    var purplecoatToggleData = this.data("purplecoat");
+    var purplecoatColorData = this.data("purplecoat-color");
+    var target = this;
+
+    f(purplecoatToggleData,
+      purplecoatColorData,
+      target);
+  }
+
+  $(function () {
+
+    var styles = '.purplecoat { display: none; position: absolute; padding: 5px; box-sizing: border-box; background-color: rgba(142, 68, 173, 0.9); color: #FFF; text-align: center; font-weight: bold; overflow: hidden; z-index: 9999; } .purplecoat-inner { display: table; width: 100%; height: 100%; } .purplecoat-inner-text { display: table-cell; vertical-align: middle;}';
+
+    var $style = $('<style/>');
+    $style.html(styles);
+    $('head').prepend($style);
+
+    $('[data-purplecoat-toggle]').on('click', function () {
+      var purplecoatToggleData = $(this).data('purplecoat-toggle');
+      var purplecoatColorData = $(this).data('purplecoat-color');
+      var target = $("[data-purplecoat=" + purplecoatToggleData + "]");
+
+      f(purplecoatToggleData,
+        purplecoatColorData,
+        target);
+    });
+  });
+})(jQuery);
